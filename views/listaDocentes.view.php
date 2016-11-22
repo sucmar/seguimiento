3e4e21e25b5f581ffb1912c6c67405160b166118
estalin <estalinyerel123@gmail.com>
2016-11-22 15:51:10 -0400

    cambios echos

---
+++ views/listaDocentes.view.php
@@ -0,0 +1,115 @@
+<?php include('views/global/header.view.php')?>
+
+<?php include('views/global/title.view.php')?>
+
+ <div class="container nt-plantel-doc ">
+    <div class="container col-md-12 table table-hover">
+    <table class="table table-hover">
+      <LEGEND>Docentes de Tiempo Completo</LEGEND>
+        <thead class="tb-head">
+         <tr>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>  <th scope="row">  1 </th> <td>  Ing. Ayoroa Cardozo Jose Richard  </td> </tr>
+          <tr>  <th scope="row">  2 </th> <td>  Msc. Costas Jáuregui Vladimir </td> </tr>
+          <tr>  <th scope="row">  3 </th> <td>  Msc. Lic. Jaldin Rosales K. Rolando </td> </tr>
+          <tr>  <th scope="row">  4 </th> <td>  Msc. Lic. Montecinos Choque Marco Antonio </td> </tr>
+          <tr>  <th scope="row">  5 </th> <td>  Msc. Ing. Orellana Araoz Jorge Walter </td> </tr>
+          <tr>  <th scope="row">  6 </th> <td>  Msc. Lic. Rodriguez Bilbao Erika Patricia </td> </tr>
+          <tr>  <th scope="row">  7 </th> <td>  Msc. Lic. Salazar Serrudo Carla </td> </tr>
+          <tr>  <th scope="row">  8 </th> <td>  Msc. Lic. Torrico Bascopé Rosemary  </td> </tr>
+          <tr>  <th scope="row">  9 </th> <td>  Ing. Villarroel Novillo Jimmy </td> </tr>
+        </tbody>
+      </table>
+      </div>
+      <br><br>
+      <div class="container col-md-12 table table-hover">
+      <table class="table table-hover">
+      <LEGEND>Docentes de Tiempo Parcial</LEGEND>
+      <thead class="tb-head">
+          <tr>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>  <th scope="row">  1 </th> <td>  Ing. Achá Perez Samuel Roberto  </td> </tr>
+          <tr>  <th scope="row">  2 </th> <td>  Ing. Agreda Corrales Luis Roberto </td> </tr>
+          <tr>  <th scope="row">  3 </th> <td>  Msc. Aparicio Yuja Nancy Tatiana  </td> </tr>
+          <tr>  <th scope="row">  4 </th> <td>  Lic. Aranibar La Fuente Ligia Jacqueline  </td> </tr>
+          <tr>  <th scope="row">  5 </th> <td>  Ing. Arispe Santander Walter  </td> </tr>
+          <tr>  <th scope="row">  6 </th> <td>  Lic. Azero Alcocer Pablo Ramon  </td> </tr>
+          <tr>  <th scope="row">  7 </th> <td>  Lic. Blanco Coca Maria Leticia  </td> </tr>
+          <tr>  <th scope="row">  8 </th> <td>  Lic. Bustillos Vargas Alex Israel </td> </tr>
+          <tr>  <th scope="row">  9 </th> <td>  Lic. Calancha Navia Boris Marcelo </td> </tr>
+          <tr>  <th scope="row">  10  </th> <td>  Msc. Camacho del Castillo Indira Elva </td> </tr>
+          <tr>  <th scope="row">  11  </th> <td>  Lic. Carrasco Calvo Alvaro Hernando </td> </tr>
+          <tr>  <th scope="row">  12  </th> <td>  Lic. Castro Lazarte Cecilia Beatriz </td> </tr>
+          <tr>  <th scope="row">  13  </th> <td>  Lic. Catari Rios Raul </td> </tr>
+          <tr>  <th scope="row">  14  </th> <td>  Lic. Cespedes Guizada Maria Benita  </td> </tr>
+          <tr>  <th scope="row">  15  </th> <td>  Lic. Choque Flores Alex D'anchgelo  </td> </tr>
+          <tr>  <th scope="row">  16  </th> <td>  Lic. Choque Uno Francisco </td> </tr>
+          <tr>  <th scope="row">  17  </th> <td>  Ing. Cosio Papadopolis Carlos J. Alfredo  </td> </tr>
+          <tr>  <th scope="row">  18  </th> <td>  Msc. Ing. Cossio Cabrera Walter </td> </tr>
+          <tr>  <th scope="row">  19  </th> <td>  Lic. Cussi Nicolas Grover </td> </tr>
+          <tr>  <th scope="row">  20  </th> <td>  Lic. Davalos Brozovic Jorge </td> </tr>
+          <tr>  <th scope="row">  21  </th> <td>  Lic. Delgadillo Cossio David Alfredo  </td> </tr>
+          <tr>  <th scope="row">  22  </th> <td>  Lic. Escalera Fernandez David </td> </tr>
+          <tr>  <th scope="row">  23  </th> <td>  Lic. Fernandez León Juan A. </td> </tr>
+          <tr>  <th scope="row">  24  </th> <td>  Ing. Fernandez Ramos David  </td> </tr>
+          <tr>  <th scope="row">  25  </th> <td>  Msc. Ing. Fiorilo Lozada Americo  </td> </tr>
+          <tr>  <th scope="row">  26  </th> <td>  Lic. Flores Soliz Juan Marcelo  </td> </tr>
+          <tr>  <th scope="row">  27  </th> <td>  Lic. Flores Villarroel Corina Justina </td> </tr>
+          <tr>  <th scope="row">  28  </th> <td>  Ing. Garcia Molina Juan Ruben </td> </tr>
+          <tr>  <th scope="row">  29  </th> <td>  Lic. Garcia Perez Carmen Rosa </td> </tr>
+          <tr>  <th scope="row">  30  </th> <td>  Lic. Grilo Salvatierra Maria Estela </td> </tr>
+          <tr>  <th scope="row">  31  </th> <td>  Ing. Gutierrez Andrade Osvaldo Walter </td> </tr>
+          <tr>  <th scope="row">  32  </th> <td>  Lic. Gutierrez Martinez Victor  </td> </tr>
+          <tr>  <th scope="row">  33  </th> <td>  Ing. Guzman Orellana Gonzalo E. Antonio </td> </tr>
+          <tr>  <th scope="row">  34  </th> <td>  Ing. Herrera Acebey Johnny  </td> </tr>
+          <tr>  <th scope="row">  35  </th> <td>  Lic. Hoepfner Reynolds Mauricio </td> </tr>
+          <tr>  <th scope="row">  36  </th> <td>  Ing. Juchani Bazualdo Demetrio  </td> </tr>
+          <tr>  <th scope="row">  37  </th> <td>  Ing. Leon Romero Gualberto  </td> </tr>
+          <tr>  <th scope="row">  38  </th> <td>  Msc. Ing. León Romero Ruperto </td> </tr>
+          <tr>  <th scope="row">  39  </th> <td>  Msc. Ing. Lima Vacaflor Tito Anibal </td> </tr>
+          <tr>  <th scope="row">  40  </th> <td>  Lic. Lucano Lucano Marcelo Javier </td> </tr>
+          <tr>  <th scope="row">  41  </th> <td>  Ing. Magariños Villarroel Mabel Gloria  </td> </tr>
+          <tr>  <th scope="row">  42  </th> <td>  Ing. Manchego Castellon Roberto Juan  </td> </tr>
+          <tr>  <th scope="row">  43  </th> <td>  Lic. Manzur Soria Carlos Benito </td> </tr>
+          <tr>  <th scope="row">  44  </th> <td>  Lic. Martinez Maida Amilcar Saul  </td> </tr>
+          <tr>  <th scope="row">  45  </th> <td>  Ing. Medina Gamboa Julio  </td> </tr>
+          <tr>  <th scope="row">  46  </th> <td>  Lic. Mejia Urquieta Victor R. </td> </tr>
+          <tr>  <th scope="row">  47  </th> <td>  Lic. Montaño Quiroga Victor Hugo  </td> </tr>
+          <tr>  <th scope="row">  48  </th> <td>  Msc. Lic. Montoya Burgos Yony Richard </td> </tr>
+          <tr>  <th scope="row">  49  </th> <td>  Ing. Omonte Ojalvo Jose Gil </td> </tr>
+          <tr>  <th scope="row">  50  </th> <td>  Ing. Omonte Ojalvo Jose Roberto </td> </tr>
+          <tr>  <th scope="row">  51  </th> <td>  Ing. Patiño Tito Ronald Edgar </td> </tr>
+          <tr>  <th scope="row">  52  </th> <td>  Lic. Peeters Ilonaa Magda Lena  </td> </tr>
+          <tr>  <th scope="row">  53  </th> <td>  Msc. Ing. Perez Fuentes Omar David  </td> </tr>
+          <tr>  <th scope="row">  54  </th> <td>  Ing. Pericon Balderrama Alfredo </td> </tr>
+          <tr>  <th scope="row">  55  </th> <td>  Ing. Quiroz Chavez Abdon  </td> </tr>
+          <tr>  <th scope="row">  56  </th> <td>  Ing. Rodriguez Sejas Juan Antonio </td> </tr>
+          <tr>  <th scope="row">  57  </th> <td>  Ing. Rojas Zurita Ramiro  </td> </tr>
+          <tr>  <th scope="row">  58  </th> <td>  Msc. Lic. Romero Rodríguez Patricia Elizabeth </td> </tr>
+          <tr>  <th scope="row">  59  </th> <td>  Lic. Salazar Anaya Rose Mary  </td> </tr>
+          <tr>  <th scope="row">  60  </th> <td>  Ing. Sarmiento Franco Ariel Antonio </td> </tr>
+          <tr>  <th scope="row">  61  </th> <td>  Lic. Silva Murillo Roxana </td> </tr>
+          <tr>  <th scope="row">  62  </th> <td>  Lic. Soruco Maita Jose Antonio  </td> </tr>
+          <tr>  <th scope="row">  63  </th> <td>  Lic. Taborga Acha Fidel </td> </tr>
+          <tr>  <th scope="row">  64  </th> <td>  Lic. Taylor Terrazas Darlong Howard </td> </tr>
+          <tr>  <th scope="row">  65  </th> <td>  Ing. Terrazas Lobo Juan </td> </tr>
+          <tr>  <th scope="row">  66  </th> <td>  Lic. Ustariz Vargas Hernan  </td> </tr>
+          <tr>  <th scope="row">  67  </th> <td>  Ing. Valenzuela Miranda Roberto </td> </tr>
+          <tr>  <th scope="row">  68  </th> <td>  Ing. Vallejos Camacho Marco Antonio </td> </tr>
+          <tr>  <th scope="row">  69  </th> <td>  Ing. Vargas Antezana Ademar Marcelo </td> </tr>
+          <tr>  <th scope="row">  70  </th> <td>  Lic. Vargas Colque Aidée  </td> </tr>
+          <tr>  <th scope="row">  71  </th> <td>  Lic. Villarroel Tapia Henrry Frank  </td> </tr>
+          <tr>  <th scope="row">  72  </th> <td>  Lic. Villazon Alcocer Christian </td> </tr>
+          <tr>  <th scope="row">  73  </th> <td>  Ing. Zabalaga Montano Oscar A </td> </tr>
+        </tbody>
+        </table>
+  </div>
+  </div>
--- views/secretaria.view.php
+++ views/secretaria.view.php
@@ -3,64 +3,120 @@
 <?php include('views/global/title.view.php')?>
 
 
-<a href='cerrar.php'>Cerar sesion</a>
+  <div class="container nt-menu-cuerpo">
+    <div class="row navbar">
 
-<div class="container nt-menu-cuerpo">
-		<div class="row navbar">
-			<div class="col-sm-2">
-				<a href="registroAuxiliar.php"><img class="img-circle img-responsive" src="images/nombramiento.png">Nombramiento</a>
-			</div>
-			<div class="col-sm-2">
-				<a href=""><img class="img-circle img-responsive" src="images/seguimiento.png">Seguimiento</a>
-			</div>
-			<div class="col-sm-2">
-				<a href=""><img class="img-circle img-responsive" src="images/reportes.png">Reportes</a>
-			</div>
-			<div class="col-sm-2">
-				<a href=""><img class="img-circle img-responsive" src="images/historicos.png">Historicos</a>
-			</div>
-			<div class="col-sm-2">
-				<a href=""><img class="img-circle img-responsive" src="images/catalogos.png">Catalogos</a>
-			</div>
-		</div>
-	</div>
 
-	<div class="container nt-menu-pie">
-		<div class="row">
-			<ul>
+      <div class="col-sm-2 dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown"><img class="img-circle img-responsive" src="images/nombramiento.png"  id="dropdownMenu1">Nombramiento</a>
 
-				<li>
-					<a href="">
-					<i class="fa fa-file-pdf-o fa-2x" aria-hidden="true"></i>
-manualPDF
-					</a>
-				</li>
+        <span class="caret"></span>
+        <ul class="dropdown-menu inline-block" role="menu" aria-labelledby="dropdownMenu1">
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Docente</a></li>
 
-				<li>
-					<a href="">
-					<i class="fa fa-file-text-o fa-2x" aria-hidden="true"></i>
-manualDOC
-					</a>
-				</li>
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Auxliar</a></li>
 
-				<li>
-					<a href="">
-					<i class="fa fa-users fa-2x" aria-hidden="true"></i>
-contacto
-					</a>
-				</li>
+        </ul>
 
-				<li>
-					<a href="">
-					<i class="fa fa-question-circle fa-2x" aria-hidden="true"></i>
-varios
-					</a>
-				</li>
+      </div>
 
-			</ul>
 
-		</div>
-	</div>
+
+      <div class="col-sm-2 dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown"><img class="img-circle img-responsive" src="images/seguimiento.png"  id="dropdownMenu1">Seguimiento</a>
+
+        <span class="caret"></span>
+        <ul class="dropdown-menu inline-block" role="menu" aria-labelledby="dropdownMenu1">
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Docente</a></li>
+
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Auxliar</a></li>
+
+        </ul>
+
+      </div>
+
+
+
+      <div class="col-sm-2 dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown"><img class="img-circle img-responsive" src="images/reportes.png"  id="dropdownMenu1">Reportes</a>
+
+        <span class="caret"></span>
+        <ul class="dropdown-menu inline-block" role="menu" aria-labelledby="dropdownMenu1">
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Docente</a></li>
+
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Auxliar</a></li>
+
+        </ul>
+
+      </div>
+      
+
+      <div class="col-sm-2 dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown"><img class="img-circle img-responsive" src="images/historicos.png"  id="dropdownMenu1">Históricos</a>
+
+        <span class="caret"></span>
+        <ul class="dropdown-menu inline-block" role="menu" aria-labelledby="dropdownMenu1">
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Docente</a></li>
+
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="">Auxliar</a></li>
+
+        </ul>
+
+      </div>
+      
+
+      <div class="col-sm-2 dropdown">
+        <a href="" class="dropdown-toggle" data-toggle="dropdown"><img class="img-circle img-responsive" src="images/registros.png">Registros</a>
+
+          <span class="caret"></span>
+          <ul class="dropdown-menu inline-block" role="menu" aria-labelledby="dropdownMenu1">
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="registroDocente.php">Registro Docente</a></li>
+
+            <li role="presentation"><a role="menuitem" tabindex="-1" href="registroAuxiliar.php">Registro Auxiliar</a></li>
+
+          </ul>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="container nt-menu-pie">
+    <div class="row">
+      <ul>
+
+        <li>
+          <a href="">
+          <i class="fa fa-file-pdf-o fa-2x" aria-hidden="true"></i>
+          ManualPDF
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+          <i class="fa fa-file-text-o fa-2x" aria-hidden="true"></i>
+          ManualDOC
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+          <i class="fa fa-users fa-2x" aria-hidden="true"></i>
+          Contacto
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+          <i class="fa fa-question-circle fa-2x" aria-hidden="true"></i>
+          Varios
+          </a>
+        </li>
+
+      </ul>
+        
+    </div>
+  </div>
+
 
 
 <?php include('views/global/subtitle.view.php')?>
