b002476f6ec2d835e3c53a5e07f94ddd7acbfa21
jjohxx <johxgks@gmail.com>
2016-11-29 16:45:25 -0400

    Fecha y hora implementada

--- views/vistaPrincipal.view.php
+++ views/vistaPrincipal.view.php
@@ -49,7 +49,7 @@ div.nt-menu-titulo div.row div.col-sm-4 h4.titulo {
 </style>
 
 <div class="container">
-
+  <div id="timer"></div>
   <div class="container text-center nt">
       <ul class="breadcrumb center-block">
           <li><a href="#">Inicio</a></li>
@@ -73,10 +73,52 @@ div.nt-menu-titulo div.row div.col-sm-4 h4.titulo {
   window.onload = function () {
     document.getElementById('button').onclick = function () {
       location.href = 'login.php';
-    }
+    };
+    setInterval(function() {
+    var currentTime = new Date ( );    
+    var currentHours = currentTime.getHours ( );   
+    var currentMinutes = currentTime.getMinutes ( );   
+    var currentSeconds = currentTime.getSeconds ( );
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;   
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;    
+    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";    
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;    
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+    var dd = currentTime.getDate();
+    var mm = currentTime.getMonth(); //January is 0!
+    var yyyy = currentTime.getFullYear();
+    var month = new Array();
+    month[0] = "enero";
+    month[1] = "febrero";
+    month[2] = "marzo";
+    month[3] = "abril";
+    month[4] = "mayo";
+    month[5] = "junio";
+    month[6] = "julio";
+    month[7] = "agosto";
+    month[8] = "septiembre";
+    month[9] = "octubre";
+    month[10] = "noviembre";
+    month[11] = "diciembre";
+    var n = month[mm]; 
+    if(dd<10) {
+        dd='0'+dd
+    } 
+    if(mm<10) {
+        mm='0'+mm
+    } 
+    var today = dd+' de '+n+' del '+yyyy;   
+    var currentTimeString = today + " - " + currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+    document.getElementById("timer").innerHTML = currentTimeString;
+}, 1000);
   } 
 </script>
 <style type="text/css">
+  div#timer {
+    font-size: 18px;
+    color: #337ab7;
+
+  }
 
   div.nt ul.breadcrumb {
     margin-top: 10px;
