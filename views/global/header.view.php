5425cc6922760e0765aa45d4153f6efa9013dfd9
sucmar <mabsbaltazar@gmail.com>
2016-12-09 16:19:29 -0400

    formulario materias completo

--- views/global/header.view.php
+++ views/global/header.view.php
@@ -25,6 +25,8 @@
     <link rel="stylesheet" type="text/css" href="estilos/css/editarPerfil.css">
     <link rel="stylesheet" type="text/css" href="estilos/css/seguimiento.css">
     <link rel="stylesheet" type="text/css" href="estilos/css/nombramiento.css">
+    <link rel="stylesheet" type="text/css" href="estilos/css/registroMaterias.css">
+
     <!--FIN HOJAS DE ESTILO-->
 
     <!---->
--- views/registroMaterias.view.php
+++ views/registroMaterias.view.php
@@ -1,7 +1,58 @@
 <?php include("views/global/header.view.php")?>
 
-<?php include('views/global/title.view.php')?>
 
-<div>
-Hola .......
+<div class="nt-registroMaterias" >
+    <fieldset>
+    <legend>REGISTRO DE MATERIAS:</legend>
+        
+        <form>
+            
+            <div class="form-group col-sm-6">
+                <label>(*) Nombre Materia:</label>
+                <input class="form-control input-global" type="text">
+            
+            </div>
+            
+            <div class="form-group col-sm-6">
+                <label>(*) Sigla Materia:</label>
+                <input class="form-control input-global" type="text">
+            </div>
+            
+            <div class="form-group col-sm-6">
+                <label>(*) Tipo Materia:</label>
+                <select class="form-control select-global">
+                    <option>CURRICULAR</option>
+                    <option>NO CURRICULAR</option>
+                </select>
+            </div>
+            
+            <div class="form-group col-sm-6">
+                <label>(*) Nivel Materia:</label>
+                <select class="form-control select-global">
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>D</option>
+                    <option>E</option>
+                    <option>F</option>
+                    <option>G</option>
+                    <option>H</option>
+                    <option>I</option>
+                    <option>J</option>
+
+                </select>
+            </div>
+            
+            <p>NOTA: Todos los campos con (*) deben ser llenados obligatoriamente</p>
+            <center>
+                    <div class="btn-inline">
+                    <button class="btn btn-default btn-global" type="button">Guardar</button>
+                    <button class="btn btn-default btn-global" type="button" >Cancelar</button>
+                    </div>
+            </center>
+            
+            
+        </form>
+        
+    </fieldset>
 </div>
