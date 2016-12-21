89a798f74665c2e261a1fcaaf40eb036ce14e51d
JohnS7687 <john.saint6078@gmail.com>
2016-12-21 17:15:36 -0430

    añadido actualizar aula

---
+++ actualizarAula.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'funciones.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $id = $_REQUEST['id'];
+    $nombre = $_POST['nom-aula'];
+    $descripcion = $_POST['des-aula'];
+    $nombre = strtoupper($nombre);
+    $descripcion = strtoupper($descripcion);
+    echo "".$nombre ." ".$descripcion;
+
+    $conexion = conexion('bd_seguimiento', 'root', '');
+    if (!$conexion) {
+        die();
+    }
+    else
+    {
+    	//consulta buscando si existe el $aula 
+                $sql1 = "SELECT * FROM aula WHERE NOMBRE_AULA='$nombre'" ;
+
+                $consulta =$conexion->prepare($sql1);
+                $consulta->execute();
+
+                if ($consulta->rowCount() == '1')
+                {   
+                    echo "este aula ya existe";
+
+                    
+                }else
+                {
+
+                    $sql = "UPDATE aula SET NOMBRE_AULA='$nombre' , DESCRIPCION_AULA='$descripcion' WHERE ID_AULA='$id'";
+                     $statement = $conexion->prepare($sql);
+  					 $statement->execute();
+    				echo $statement->rowCount(). 'record update';
+    				 header('Location: registrarAula.php');
+
+                }   
+               
+    }
+    
+   
+
+    //echo $id,$nombres;
+}
