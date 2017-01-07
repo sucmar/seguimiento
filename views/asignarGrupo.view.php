75c176037a3d65044525e92957d5524706ee9e4b
estalin <estalinyerel123@gmail.com>
2017-01-07 01:39:44 -0400

    vistas mejoradas docente, mostrar materias

--- views/asignarGrupo.view.php
+++ views/asignarGrupo.view.php
@@ -2,18 +2,22 @@
 
 <?php include('views/global/title.view.php')?>
 
-
-<form class="form-inline" action="establecerGrupo.php?idDoc=<?php echo $idDocMateria ?>&idMa=<?php echo $idMateria?>&idDocente=<?php echo $idDoc?>" method="post">
-    <div class="form-group div-form-ded col-md-3">
-        <label for="lab-ded">Eliga un grupo:</label>
-        <select class="form-control select-global" name="grup">
-            <?php foreach ($grupos as $grupo): ?>
-                <option  value="<?php echo  $grupo['ID_GRUPO'] ?>"><?php  echo  $grupo['NOM_GRUPO'] ?></option>
-            <?php endforeach;?>
-        </select>
-    </div>
-    <div>
-        <input type="submit" class="btn registrar btn-global" name="" value="selecionar">
-        <button type="button" onclick="history.back()" class="btn cancelar btn-global" >Cancelar</button>
-    </div>
-</form>
+<div class="container nt-form-asignar-grupo ">
+    <form class="form-inline" action="establecerGrupo.php?idDoc=<?php echo $idDocMateria ?>&idMa=<?php echo $idMateria?>&idDocente=<?php echo $idDoc?>" method="post">
+        <legend>Grupos que tiene la Materia</legend>
+        <div class="form-group div-form-ded col-md-8 col-md-offset-2">
+            <label for="lab-ded">Eliga un grupo:</label>
+            <select class="form-control select-global" name="grup">
+                <?php foreach ($grupos as $grupo): ?>
+                    <option  value="<?php echo  $grupo['ID_GRUPO'] ?>"><?php  echo  $grupo['NOM_GRUPO'] ?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+        <br>
+        <br>
+        <div class="col-md-8 col-md-offset-2" >
+            <input type="submit" class="btn registrar btn-global" name="" value="selecionar">
+            <button type="button" onclick="history.back()" class="btn cancelar btn-global" >Cancelar</button>
+        </div>
+    </form>
+</div>
--- views/gruposAsignados.view.php
+++ views/gruposAsignados.view.php
@@ -1,7 +1,7 @@
 <?php include('views/global/header.view.php')?>
 <?php include('views/global/title.view.php')?>
 
-
+<div class="container nt-form-grupos-asignados">
 <table  class="table table-bordered table-hover" id="table">
     <thead class="thead-inverse">
     <tr class="header">
@@ -15,10 +15,8 @@
             <td id="ideMateria"><?php echo $materia['NOMBRE_MATERIA'] ?></td>
             <td id="nombreMateria"><?php echo $materia['GRUPO'] ?></td>
             <td><a href="horarioMateria.php?idDoc=<?php echo $materia['ID_DOCENTE']?>&idMateria=<?php echo $materia['ID_MATERIA']?>&grupo= <?php echo $materia['ID_GRUP']?>" class="btn btn-link">asignar horario</a></td>
-            <td><a href="eliminarGrupo.php?idDocMateria=<?php echo $materia['ID_DOCMATERIA']?>&idMateria=<?php echo $materia['ID_MATERIA']?>&grupo= <?php echo $materia['ID_DOCENTE']?>" class="btn btn-link">Eliminar Grupo</a></td>
         </tr>
     <?php endforeach;?>
 </table>
+</div>
 <td><a href="docente.php?id=<?php echo $idDoc?>" class="btn btn-link">Volver</a></td>
-
-
