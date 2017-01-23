867dfe693692fbb9abeeca8d2882f1f3b1002090
sucmar <mabsbaltazar@gmail.com>
2017-01-23 12:53:58 -0400

    cambios modificar materia aniadido

--- modificarCarrera.php
+++ modificarCarrera.php
@@ -7,7 +7,7 @@ $conexion = conexion('bd_seguimiento','seg_user', 'seg_pass');
 if (!$conexion) {
     die();
 }
-
+	
 $id = $_REQUEST['id'];
 //echo $id;
 try{
@@ -15,9 +15,23 @@ try{
     $statement->execute();
     $carreras = $statement->fetch(PDO::FETCH_ASSOC); // capturar datos cada columna de una consulta dada
 	
-	$statement = $conexion->prepare("SELECT NOMBRE_FACULTAD FROM facultad WHERE ID_FACULTAD = '$id'");
-    $statement->execute();
-    $facultades = $statement->fetch(PDO::FETCH_ASSOC); // capturar datos cada columna 
+	$statement2 = $conexion->prepare("SELECT ID_FACULTAD FROM carrera WHERE ID_CARRERA = '$id'");
+    $statement2->execute();
+    $idfacu = $statement2->fetch(PDO::FETCH_ASSOC); // capturar datos cada columna de una consulta 
+    $idf=$idfacu['ID_FACULTAD'];
+	
+	$statement5 = $conexion->prepare("SELECT * FROM facultad WHERE ID_FACULTAD = '$idf'");
+    $statement5->execute();
+    $facultades = $statement5->fetch(PDO::FETCH_ASSOC); // capturar datos cada columna 
+
+    $statement3 = $conexion->prepare("SELECT ID_DPTO FROM carrera WHERE ID_CARRERA = '$id'");
+    $statement3->execute();
+    $iddpto = $statement3->fetch(PDO::FETCH_ASSOC); // capturar datos cada columna de una consulta 
+    $idd=$iddpto['ID_DPTO'];
+
+    $statement4 = $conexion->prepare("SELECT * FROM departamento WHERE ID_DPTO = '$idd'");
+    $statement4->execute();
+    $departamentos = $statement4->fetch(PDO::FETCH_ASSOC); // capturar datos cada columna de una consulta 
 
 }catch(PDOException $e) {
     echo $e->getMessage();
---
+++ views/modificarMateria.view.php
@@ -0,0 +1,142 @@
+<?php include('views/global/header.view.php')?>
+<?php include('views/global/title.view.php')?>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<div class="nt-registroMaterias" >
+    <fieldset>
+    <legend>REGISTRO DE MATERIAS:</legend>
+        
+        <form action="actualizarMateria.php?id=<?php echo $materias['ID_MATERIA'] ?>" method="POST" onsubmit="return validate();">
+           			
+			<div class="form-group col-sm-12">
+                <label>(*) Carrera:</label>
+                <select class="form-control select-global" name="nombreCarrera">
+                    <?php foreach ($carreras as $carrera):?>
+                        <option><?php echo $carrera['NOMBRE_CARRERA'] ?></option>
+                    <?php endforeach;?>
+                </select>
+            
+            </div>
+
+			<div class="form-group col-sm-6">
+                <label>(*) Nombre Materia:</label>
+			    <input class="form-control input-global" type="text" id="nombreMateria" name="nombreMateria" value="<?php echo $materias['NOMBRE_MATERIA']; ?>" required>
+            </div>
+            
+            <div class="form-group col-sm-6">
+                <label>(*) Sigla Materia:</label>
+				<input class="form-control input-global" type="text" id="siglaMateria" name="siglaMateria" value="<?php echo $materias['SIGLA_MATERIA']; ?>" required>
+            </div>
+			
+			<div class="form-group col-sm-12">
+                <label>(*) Nombre Departamento:</label>
+                <select class="form-control select-global" name="nombreDpto">
+                    <?php foreach ($departamentos as $departamento):?>
+                        <option><?php echo $departamento['NOMBRE_DPTO'] ?></option>
+                    <?php endforeach;?>
+                </select>
+            </div>
+			
+			<div class="form-group col-sm-4">
+                <label>(*) Carga Horaria Mes:</label>
+                <select class="form-control select-global" id="cargaHorariaMateria" name="cargaHorariaMateria">
+                    <option>16</option>
+                    <option>24</option>
+                    <option>32</option>
+                </select>
+            </div>
+            
+            <div class="form-group col-sm-4">
+                <label>(*) Tipo Materia:</label>
+                <select class="form-control select-global" id="tipoMateria" name="tipoMateria">
+                    <option>CURRICULAR</option>
+                    <option>NO CURRICULAR</option>
+                </select>
+            </div>
+            
+            <div class="form-group col-sm-4">
+                <label>(*) Nivel Semestral Materia:</label>
+                <select class="form-control select-global" id="nivelMateria" name="nivelMateria">
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="C">C</option>
+                    <option value="D">D</option>
+                    <option value="E">E</option>
+                    <option value="F">F</option>
+                    <option value="G">G</option>
+                    <option value="H">H</option>
+                    <option value="I">I</option>
+                    <option value="J">J</option>
+
+                </select>
+            </div>
+            
+            
+            
+            <p>NOTA: Todos los campos con (*) deben ser llenados obligatoriamente</p>
+            <p id="error_de"> Enter a number and click OK:</p>
+
+            <center>
+                    <div class="btn-inline">
+                    <button class="btn btn-default btn-global" type="submit">Guardar</button>
+                    <a class="btn btn-default btn-global" href="listaMaterias.php">Cancelar</a>
+                    </div>
+            </center>
+            
+            
+        </form>
+        
+    </fieldset>
+</div>
+
+<script>
+
+function validate()
+    {
+        var error="";
+        var nombres = document.getElementById( "nombreMateria" );
+        var letters = /^[A-Za-z- -]+$/;
+        var numbers = /^[1-9-0]+$/;
+                
+        if( nombres.value == "" || !nombres.value.match(letters))
+        {
+            error = " Nombre de carrera no deberia contener numeros o caracteres especiales ";
+            document.getElementById( "error_de" ).style.color = "red";
+            document.getElementById( "error_de" ).innerHTML = error;
+            return false;
+        }
+        var sigla = document.getElementById( "siglaMateria" );
+        if( sigla.value == "" || !sigla.value.match(numbers) )
+        {
+            error = " Tienes que escribir una sigla numeral para la Carrera ";
+            document.getElementById( "error_de" ).style.color = "red";
+            document.getElementById( "error_de" ).innerHTML = error;
+            return false;
+        }
+        var tipo = document.getElementById( "tipoMateria" );
+        if( tipo.value == "" || !tipo.value.match(letters) )
+        {
+            error = " Tienes que escribir una sigla para la Carrera ";
+            document.getElementById( "error_de" ).style.color = "red";
+            document.getElementById( "error_de" ).innerHTML = error;
+            return false;
+        }
+        var nivel = document.getElementById( "nivelMateria" );
+        if( nivel.value == "" || !nivel.value.match(letters) )
+        {
+            error = " Tienes que escribir una sigla para la Carrera ";
+            document.getElementById( "error_de" ).style.color = "red";
+            document.getElementById( "error_de" ).innerHTML = error;
+            return false;
+        }
+        else
+        {
+            error = " Datos insertados correctamente ";
+            document.getElementById( "error_de" ).style.color = "blue";
+            document.getElementById( "error_de" ).innerHTML = error;
+            return true;
+        }
+    }
+</script>
+
